419bb63bd351f74e9344b8d8f39de49de7e4fd10
WUALTER AUGUSTO HERNÁNDEZ <141308810+2002August@users.noreply.github.com>
2023-10-22 12:09:17 -0600

    Add files via upload

---
+++ Proyecto_final/src/java/modelo/proveedor.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author augus
+ */
+public class proveedor {
+    public int idproveedor;
+    public String proveedor,nit,direccion,telefono;
+
+    public proveedor(int idproveedor, String proveedor, String nit, String direccion, String telefono) {
+        this.idproveedor = idproveedor;
+        this.proveedor = proveedor;
+        this.nit = nit;
+        this.direccion = direccion;
+        this.telefono = telefono;
+    }
+
+    public int getIdproveedor() {
+        return idproveedor;
+    }
+
+    public void setIdproveedor(int idproveedor) {
+        this.idproveedor = idproveedor;
+    }
+
+    public String getProveedor() {
+        return proveedor;
+    }
+
+    public void setProveedor(String proveedor) {
+        this.proveedor = proveedor;
+    }
+
+    public String getNit() {
+        return nit;
+    }
+
+    public void setNit(String nit) {
+        this.nit = nit;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    
+}
